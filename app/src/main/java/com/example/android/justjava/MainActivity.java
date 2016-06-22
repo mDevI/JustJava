ccb3ed940c57910db143ccfd546c894145e463ea
mDevI <2belonosov@gmail.com>
2016-06-22 21:55:30 +0700

    Realyze  the ORDER SUMMARY textView logic.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -11,8 +11,8 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int numberOfCoffees;
-    private String message;
+    int numberOfCoffees; // variable for count number cups of coffee.
+    private String message; // for output message.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,26 +47,30 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        message = "\nThank you!";
+//       message = "\nThank you!";
+
         displayMessage(message, numberOfCoffees*5 );
-        // display(numberOfCoffees);
-        // displayPrice(numberOfCoffees*5);
+
     }
 
     /**
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        priceTextView.setText("Total: " + NumberFormat.getCurrencyInstance().format(number));
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText("Total: " + NumberFormat.getCurrencyInstance().format(number));
     }
 
     /**
      * This method displays the given message on the screen.
      */
-    private void displayMessage(String message, int number) {
+    private void displayMessage(String message, int Total) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText("Total: " + NumberFormat.getCurrencyInstance().format(number) + message);
+        message = "Name:\t\tCaptain Kunal";
+        message += "\nQuantity:\t" + numberOfCoffees;
+        message += "\nTotal:\t" +  NumberFormat.getCurrencyInstance().format(Total);
+        message += "\nThank you!";
+        orderSummaryTextView.setText(message );
     }
 
     /**
